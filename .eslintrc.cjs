b2299f275e042a10ad6d8988ba5f2246e632a281
Akash Gautam <akashgautam01584@gmail.com>
2026-02-20 22:19:57 +0530

    Add changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,14 +13,6 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    overrides: [
-      {
-        files: ['**/*.test.{js,jsx}'],
-        rules: {
-          'no-unused-vars': 'off',
-        },
-      },
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
